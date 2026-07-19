2815782a927bbfbbd404851822f27f62e63c4f6e
Yvheniia <evheniia@MacBookPro.fritz.box>
2026-07-19 19:49:11 +0200

    dynamic name

--- server/main.py
+++ server/main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/capital")
 def get_capital():
     return {"capital": 500}
+
+# http://127.0.0.1:8000/hello/bohdan
+
+@app.get("/hello/{name}")   # дірка 1: як назвати змінну частину адреси
+def hello(name):            # дірка 2: що функція приймає ззовні
+    return "Hello, " +name         # дірка 3: рядок "Привіт, " + ім'я
